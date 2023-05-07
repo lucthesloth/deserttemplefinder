12da474db89f1f14794df16e6e5a3afee094642a
Lucas Carvalho <14770259794@cefet-rj.br>
2023-05-07 19:19:13 -0300

    Supressing warnings, Removing potential null pointers

--- src/main/java/me/lucthesloth/deserttemplefinder/DesertTempleFinder.java
+++ src/main/java/me/lucthesloth/deserttemplefinder/DesertTempleFinder.java
@@ -5,13 +5,12 @@ import org.bukkit.Location;
 import org.bukkit.StructureType;
 import org.bukkit.block.Biome;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.server.ServerLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class DesertTempleFinder extends JavaPlugin implements Listener {
     public FileConfiguration config;
@@ -27,15 +26,13 @@ public final class DesertTempleFinder extends JavaPlugin implements Listener {
             config.set("runCheck", true);
             saveConfig();
         } else {
-            Bukkit.getScheduler().runTaskLater(this, new Runnable() {
-                @Override
-                public void run() {
-                    findTemples(config.getInt("minX", -10000), -config.getInt("minZ", -10000), config.getInt("mxnX", 10000), config.getInt("manZ", 10000));
+
+            Bukkit.getScheduler().runTaskLater(this, () -> {
+                    int total = findTemples(config.getInt("minX", -10000), -config.getInt("minZ", -10000), config.getInt("mxnX", 10000), config.getInt("manZ", 10000));
                     config.set("runCheck", false);
                     saveConfig();
-                    Bukkit.getLogger().info(String.format("Found %d temples", (config.getList("temples", Collections.emptyList())).size()));
+                    Bukkit.getLogger().info(String.format("Found %d temples", total));
 
-                }
             }, 20L);
         }
     }
@@ -44,22 +41,27 @@ public final class DesertTempleFinder extends JavaPlugin implements Listener {
     public void onDisable() {
         // Plugin shutdown logic
     }
-    private void addIfNotPresent(Location loc){
+
+    private boolean addIfNotPresent(Location loc){
         if (config.getList("temples") == null) {
             config.set("temples", new ArrayList<String>(){{
                 add(String.format("%d,%d", loc.getBlockX(), loc.getBlockZ()));
             }});
         } else {
-            ArrayList<String> temples = (ArrayList<String>) config.getList("temples");
+            @SuppressWarnings("unchecked") ArrayList<String> temples = (ArrayList<String>) config.getList("temples", Collections.emptyList());
+            assert temples != null;
             boolean found = temples.contains(String.format("%d,%d", loc.getBlockX(), loc.getBlockZ()));
             if (!found) {
                 temples.add(String.format("%d,%d", loc.getBlockX(), loc.getBlockZ()));
                 config.set("temples", temples);
                 saveConfig();
+                return true;
             }
         }
+        return false;
     }
-    private void findTemples(int minX, int minZ, int maxX, int maxZ) {
+    private int findTemples(int minX, int minZ, int maxX, int maxZ) {
+        AtomicInteger count = new AtomicInteger();
         Bukkit.getWorlds().forEach(w -> {
             Location temp;
             for (int x = minX; x < maxX; x+=16) {
@@ -67,11 +69,14 @@ public final class DesertTempleFinder extends JavaPlugin implements Listener {
                     if (w.getBiome(x, z) == Biome.DESERT) {
                         temp = w.locateNearestStructure(new Location(w, x, 65, z), StructureType.DESERT_PYRAMID, 64, false);
                         if (temp != null) {
-                            addIfNotPresent(temp);
+                            if (addIfNotPresent(temp)) {
+                                count.getAndIncrement();
+                            }
                         }
                     }
                 }
             }
         });
+        return count.get();
     }
 }
